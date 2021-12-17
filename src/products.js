7010af7e54e2293e01334e494b2c6677f7f87dad
JPedro-SG <81583744+JPedro-SG@users.noreply.github.com>
2021-12-17 14:44:00 -0300

    Add Products Routes

--- src/products.js
+++ src/products.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const router = express.Router();
-
-const pool = require('./db')
+const async = require('async')
+const pool = require('./db');
+const { waterfall } = require('async');
 
 router.get("/", (req, res) => {
     pool.query('SELECT * FROM products ORDER BY id ASC')
@@ -17,5 +18,102 @@ router.get("/:id", (req, res) => {
         .then(results => res.json(results.rows))
         .catch(err => res.json(err))
 })
+
+const insertCategoriesIntoProducts = (categories, product, cb) => {
+    let prod_categories = []
+    const insertCat = (category, callback) => {
+        pool.query("INSERT INTO prod_categories(prod_id, catg_id) VALUES ($1, $2) RETURNING *", [product.id, category])
+            .then(res => {
+                prod_categories.push(res.rows[0].catg_id)
+                callback()
+            })
+            .catch(err => console.log(err))
+    }
+
+    async.each(categories, insertCat)
+        .then(() => cb(null, {...product, categories: prod_categories}))
+        .catch(err => console.log(err))
+}
+
+router.post("/", (req, res) => {
+    async.waterfall([
+        function(callback){
+            const {name, amount, sales_avg, description} = req.body
+
+            pool.query("INSERT INTO products(name, amount, sales_avg, description) VALUES ($1, $2, $3, $4) RETURNING *", 
+                [name, amount, sales_avg, description])
+                .then(table => callback(null, table.rows[0]))
+                .catch(err => res.json({message: "Ocorreu algum erro com a inserção do produto", ...err}))         
+        },
+        function(product, callback){
+
+            const categories = req.body.categories
+            insertCategoriesIntoProducts(categories, product, callback)
+        }
+    ], 
+    
+    function (err, results) {
+        // console.log(results)
+        if(err) return res.json({message: "Ocorreu algum erro com a inserção do produto", ...err})
+        return res.json({message: "Produto inserido com sucesso!", ...results})
+    })
+    
+})
+
+router.delete("/:id", (req, res) => {
+    const {id} = req.params
+    async.series([
+        function(callback){
+            pool.query("DELETE FROM prod_categories WHERE prod_id = $1", [id])
+            callback()
+        }, 
+        function(callback){
+            pool.query("DELETE FROM products WHERE prod_id = $1", [id])
+            callback()
+        }
+    ], 
+    function(err, results) {
+        if(err) res.json({message: "Produto não se encontra na lista de produtos!", ...err})
+        res.json("Produto removido com sucesso!")
+    })
+})
+
+router.put("/:id", (req, res) => {
+    const {id} = req.params
+    const {name, amount, sales_avg, description} = req.body
+
+    async.series([
+        function(callback){
+            pool.query("UPDATE products SET name = $1, amount = $2, sales_avg = $3, description = $4 WHERE id = $5 RETURNING *", [name, amount, sales_avg, description,id])
+            callback()
+        }, 
+        function(callback){
+            pool.query("DELETE FROM prod_categories WHERE prod_id = $1", [id])
+            callback()
+        },
+        function(callback) {
+            const categories = req.body.categories
+            // console.log(categories)
+            
+            const insertCat = (category, cb) => {
+                pool.query("INSERT INTO prod_categories(prod_id, catg_id) VALUES ($1, $2)", [id, category])
+                    .then(_ => cb())
+                    .catch(err => console.log(err))
+            }
+
+            async.each(categories, insertCat)
+                .then(() => callback())
+                .catch(err => console.log(err))
+        }
+
+    ], 
+    function(err, results) {
+        if(err) res.json({message: "Ocorreu algo inesperado!", ...err})
+        res.json("Produto atualizado com sucesso!")
+    })
+
+})
+
+// UPDATE products SET name = 'Relógio Escuro', amount = 67 WHERE id = 37; to update
 
 module.exports = router
